47f3e443d887667cba27623ded8f940a9371c485
Manjiz <25199230@qq.com>
2015-12-09 19:50:54 +0800

    配置文件

---
+++ conf-server.js
@@ -0,0 +1,38 @@
+﻿/*
+ * Configuration of UFT 
+ * Created on 2015-11-12 by Manji.z
+ * Last Modified at 2015-11-12 by Manji.z
+ * Only For Server
+ */
+
+;(function() {
+	'use strict';
+
+	 var root = this;
+	 var conf = function(obj) {
+	 	return {
+	 		port: 8088,
+	 		nodemailer: {
+			    service: '163',
+			    auth: {
+			        user: '******',
+			        pass: '******'
+			    }
+			}
+	 	};
+	 }
+
+	 // -------------------------------------------------------
+	 if(typeof exports !== 'undefined') {
+	 	if(typeof module !== 'undefined' && module.exports) {
+	 		exports = module.exports = conf;
+	 	}
+	 	exports.conf = conf;
+	 } else if (typeof define === 'function' && define.amd) {
+	 	define('underscore', function() {
+	 		return conf;
+	 	})
+	 } else {
+	 	root.conf = conf;
+	 }
+}).call(this);
